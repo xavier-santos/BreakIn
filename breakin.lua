e6ef7c93b679dd56c39eb7b1153429ca1a1375fa
Educorreia932 <skelozard1@gmail.com>
2021-03-12 22:53:17 +0000

    Paddle now moves

--- breakin.lua
+++ breakin.lua
@@ -1,10 +1,15 @@
+-- title:  Breakin
+-- author: Triceratops 
+-- desc:   Game developed for Retro Jam 2021
+-- script: lua
+
 function init()
     player = {
         x = (240 /2 ) - 12,
         y = 120,
         width = 24,
         height = 4,
-        color = 3,
+        color = 1,
         speed = {
             x = 0,
             max = 4
@@ -14,16 +19,80 @@ end
 
 init()
 
-function TIC()
-   input()
-   update()
-   draw()
+function input()
+    local sx = player.speed.x
+    local smax = player.speed.max
+
+    -- Move to left
+    if btn(2) then
+        if sx>-smax then
+            sx=sx-2
+
+        else
+            sx=-smax
+
+        end	 
+    end
+
+    -- Move to right
+    if btn(3) then
+        if sx<smax then
+            sx=sx+2
+            
+        else
+            sx=smax
+
+        end
+    end
+
+    player.speed.x = sx
+    player.speed.max = smax
+end
+
+function update()
+    local px = player.x
+    local psx = player.speed.x
+    local smax = player.speed.max
+
+    -- Update player position
+    px = px + psx
+
+    -- Reduce player speed
+    if psx ~= 0 then
+        if psx > 0 then
+            psx=psx-1
+
+        else
+            psx=psx+1
+
+        end
+    end
+
+    player.x = px
+    player.speed.x = psx
+    player.speed.max = smax
 end
 
 function draw()
     draw_game_objects()
-    draw_gui()
+    -- draw_gui()
 end
+
+function draw_game_objects()
+    rect(player.x,
+        player.y,
+        player.width,
+        player.height,
+        player.color
+    )
+end
+
+function TIC()
+    cls()
+    input()
+    update()
+    draw()
+ end
 
 -- <TILES>
 -- 000:1111111111111111111111111111111111111111111111111111111111111111
