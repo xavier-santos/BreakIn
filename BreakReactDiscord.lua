474b4fd1624f3635621571611c29cc74ccfc9949
Miguel Xavier Santos <1181078@isep.ipp.pt>
2021-03-12 22:22:39 +0000

    Initial commit

---
+++ BreakReactDiscord.lua
@@ -0,0 +1,72 @@
+-- title:  game
+-- author: Triceratops 
+-- desc:   A game
+-- script: lua
+
+-- Encontrei este template, depois podemos adicionar coisas aqui
+
+t=0
+x=96
+y=24
+
+function init()
+    -- here we initialize all our variables and game objects
+end
+
+init()
+
+function TIC()
+    -- here we will place our main game loop
+    input()
+    update()
+    draw()
+	if btn(0) then y=y-1 end
+	if btn(1) then y=y+1 end
+	if btn(2) then x=x-1 end
+	if btn(3) then x=x+1 end
+
+	cls(13)
+	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
+	print("HELLO WORLD!",84,84)
+	t=t+1
+end
+   
+function input()
+    -- so that the player can actually do something
+end
+   
+function update()
+    -- so that something is going on
+end
+   
+function draw()
+    -- so we can see something
+end
+
+--Por amor de deus nao apaguem esta merda aqui em baixo, nem que isto mate a vossa avo
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
