fe61a0abf065eec4a08b4a9bd9a25a720335e4a5
Educorreia932 <skelozard1@gmail.com>
2021-03-13 15:37:26 +0000

    Rotating paddle with buttons 4 and 5

--- breakin.lua
+++ breakin.lua
@@ -4,6 +4,7 @@
 -- script: lua
 
 require "collisions"
+require "graphics"
 
 function init()
     -- Player
@@ -16,9 +17,10 @@ function init()
     paddle = {
         x = (240 / 2) - 12,
         y = 120,
-        width = 24,
-        height = 4,
+        width = 12,
+        height = 2,
         color = 1,
+        angle = 0,
         speed = {
             x = 0,
             max = 4
@@ -42,8 +44,8 @@ function init()
 
     -- Bricks
     bricks = {}
-    brick_count_height = 10
-    brick_count_width = 19
+    brick_count_height = 0
+    brick_count_width = 0
     
     for i = 0, brick_count_height do
         for j = 0, brick_count_width do
@@ -86,6 +88,16 @@ function input()
             sx = smax
 
         end
+    end
+
+    -- Anti-clockwise rotate
+    if btn(4) then
+        paddle.angle = paddle.angle + math.pi / 16
+    end
+
+    -- Clockwise rotate
+    if btn(5) then
+        paddle.angle = paddle.angle - math.pi / 16
     end
 
     -- Deactived ball
@@ -137,70 +149,6 @@ function update()
     paddle.speed.max = smax
 end
 
-function draw()
-    draw_game_objects()
-    -- draw_gui()
-end
-
-function draw_game_objects()
-    -- Draw paddle
-    draw_paddle()
-
-    -- Draw ball
-    rect(
-        ball.x,
-        ball.y,
-        ball.width,
-        ball.height,
-        ball.color
-    )
-
-    -- Draw bricks
-    for i, brick in pairs(bricks) do
-        rect(
-            brick.x,
-            brick.y,
-            brick.width,
-            brick.height,
-            brick.color
-        )
-    end
-end
-
-function draw_paddle()
-    x = 50
-    y = 50
-    h = 2
-    w = 6 * h
-    angle = math.pi / 3
-
-    x1, y1 = rotate(-w, h, angle)
-    x1, y1 = translate(x1, y1, x, y)
-
-    x2, y2 = rotate(-w, -h, angle)
-    x2, y2 = translate(x2, y2, x, y)
-    
-    x3, y3 = rotate(w, h, angle)
-    x3, y3 = translate(x3, y3, x, y)
-
-    x4, y4 = rotate(w, -h, angle)
-    x4, y4 = translate(x4, y4, x, y)
-
-    tri(x1, y1, x2, y2, x3, y3, 1)
-    tri(x2, y2, x3, y3, x4, y4, 1)
-end
-
-function rotate(x, y, angle) 
-    _x = x * math.cos(angle) - y * math.sin(angle)
-    _y = x * math.sin(angle) + y * math.cos(angle)
-
-    return _x, _y
-end
-
-function translate(x, y, w, h)
-    return x + w, y + h
-end
-
 function TIC()
     cls()
     input()
@@ -214,6 +162,6 @@ function TIC()
 -- </TILES>
 
 -- <PALETTE>
--- 000:140c1c44243430346d4e4a4f854c30346524d04648757161597dced27d2c8595a16daa2cd2aa996dc2cadad45edeeed6
+-- 000:140c1cfa183430346d4e4a4f854c30346524d04648757161597dced27d2c8595a16daa2cd2aa996dc2cadad45edeeed6
 -- </PALETTE>
 
---
+++ graphics.lua
@@ -0,0 +1,63 @@
+function draw()
+    draw_game_objects()
+    -- draw_gui()
+end
+
+function draw_game_objects()
+    -- Draw paddle
+    draw_paddle()
+
+    -- Draw ball
+    rect(
+        ball.x,
+        ball.y,
+        ball.width,
+        ball.height,
+        ball.color
+    )
+
+    -- Draw bricks
+    for i, brick in pairs(bricks) do
+        rect(
+            brick.x,
+            brick.y,
+            brick.width,
+            brick.height,
+            brick.color
+        )
+    end
+end
+
+function draw_paddle()
+    x = paddle.x
+    y = paddle.y
+    h = paddle.height
+    w = paddle.width
+    angle = paddle.angle
+
+    x1, y1 = rotate(-w, h, angle)
+    x1, y1 = translate(x1, y1, x, y)
+
+    x2, y2 = rotate(-w, -h, angle)
+    x2, y2 = translate(x2, y2, x, y)
+    
+    x3, y3 = rotate(w, h, angle)
+    x3, y3 = translate(x3, y3, x, y)
+
+    x4, y4 = rotate(w, -h, angle)
+    x4, y4 = translate(x4, y4, x, y)
+
+    tri(x1, y1, x2, y2, x3, y3, 1)
+    tri(x2, y2, x3, y3, x4, y4, 1)
+end
+
+function rotate(x, y, angle) 
+    _x = x * math.cos(angle) - y * math.sin(angle)
+    _y = x * math.sin(angle) + y * math.cos(angle)
+
+    return _x, _y
+end
+
+function translate(x, y, w, h)
+    return x + w, y + h
+end
